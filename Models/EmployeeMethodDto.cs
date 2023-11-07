a506ffbf369bc08a5de79fa79657cc6d7d795708
unknown <arutiunovadrian@gmail.com>
2023-11-07 21:44:17 +0500

    first commit

---
+++ Models/EmployeeMethodDto.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models;
+
+public class EmployeeMethodDto
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Department { get; set; }
+}
